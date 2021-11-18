4ebf29546a195fbe417de36ed41ad6d889d2a7fb
Deekshant Yadav <deekshant@imgglobalinfotech.com>
2021-11-18 10:06:30 +0530

    method

--- Hello/src/main/java/com/img/hello/HelloWord.java
+++ Hello/src/main/java/com/img/hello/HelloWord.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 public class HelloWord {
 
-    public static void Toasto(Context context, String message){
+    public static void HelloToasto(Context context, String message){
         Toast.makeText(context, message+"Now Toast", Toast.LENGTH_SHORT).show();
     }
 }
